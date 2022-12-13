1b610f872a245774050095876390b28817d3ada3
siva prasad <prasad230776@gmail.com>
2022-12-13 12:12:28 +0530

    Update index.py

--- index.py
+++ index.py
@@ -1,1 +1,17 @@
-from wsgi import app
+from flask import Flask, render_template
+app = Flask(__name__)
+
+@app.route("/")
+def index():
+    return render_template("index.html")
+
+@app.route("/about")
+def about():
+    return "from about"
+
+if __name__ == "__main__":
+    app.run(debug=True)
+Footer
+© 2022 GitHub, Inc.
+Footer navigation
+Terms
